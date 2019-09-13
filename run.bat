b9a90811c6df2d020be0329ce1160aab5f975292
Pedro Amaro <n101010k@gmail.com>
2019-09-13 03:29:26 +0100

    Improved network performance

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-docker run -p 7777:7777/udp -v %cd%\brikkit.conf:/app/brikkit.conf -v %cd%\plugins:/app/plugins -v %cd%\conf:/app/conf -v %cd%\saved:/app/saved -v %cd%\brickadia:/app/brickadia brikkit
+docker run --net=host -v %cd%\brikkit.conf:/app/brikkit.conf -v %cd%\plugins:/app/plugins -v %cd%\conf:/app/conf -v %cd%\saved:/app/saved -v %cd%\brickadia:/app/brickadia brikkit
