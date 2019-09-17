8f7479f6e957c1fd75145e7728f9baaef136561c
Pedro Amaro <n101010k@gmail.com>
2019-09-17 15:32:27 +0100

    Supports spaces in path now

--- bash_toolbox.bat
+++ bash_toolbox.bat
@@ -1,3 +1,3 @@
 SET cd=/c/Users/n42k/Downloads/brikkit_docker-master
-docker run -it --entrypoint /bin/bash -p 7777:7777/udp -v %cd%/brikkit.conf:/app/brikkit.conf -v %cd%/plugins:/app/plugins -v %cd%/conf:/app/conf -v %cd%/saved:/app/saved -v %cd%/logs:/app/logs -v %cd%/brickadia:/app/brickadia brikkit
+docker run -it --entrypoint /bin/bash -p 7777:7777/udp -v "%cd%/brikkit.conf:/app/brikkit.conf" -v "%cd%/plugins:/app/plugins" -v "%cd%/conf:/app/conf" -v "%cd%/saved:/app/saved" -v "%cd%/logs:/app/logs" -v "%cd%/brickadia:/app/brickadia" brikkit
 pause
